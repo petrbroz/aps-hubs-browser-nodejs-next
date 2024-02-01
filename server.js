e8478b607d5b8586c62704591cc6a73727548061
Petr Broz <petr.broz@autodesk.com>
2024-02-01 10:21:16 +0100

    Code simplifications.

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import cookieSession from 'cookie-session';
 import { getAuthorizationUrl, getCredentials, refreshCredentials, getUserProfile, getHubs, getProjects, getProjectContents, getItemVersions } from './aps.js';
 import { PORT, SERVER_SESSION_SECRET } from './config.js';
 
-async function authMiddleware(req, res) {
-    if (!req.session.refreshToken) {
+async function checkAuth(req, res) {
+    const { credentials } = req.session;
+    if (!credentials) {
         res.send(401);
-        return;
-    }
-    if (req.session.expiresAt < Date.now()) {
-        const credentials = await refreshCredentials(req.session.refreshToken);
-        Object.assign(req.session, credentials);
+    } else {
+        if (credentials.expires_at < Date.now()) {
+            req.session.credentials = await refreshCredentials(credentials.refresh_token);
+        }
     }
 }
 
@@ -31,39 +31,37 @@ server.get('/api/auth/logout', async function (req, res) {
 });
 
 server.get('/api/auth/callback', async function (req, res) {
-    const credentials = await getCredentials(req.params.code);
-    Object.assign(req.session, credentials);
+    req.session.credentials = await getCredentials(req.params.code);
     res.send(302, null, { Location: '/' });
 });
 
-server.get('/api/auth/token', authMiddleware, async function (req, res) {
-    const expiresIn = Math.round((req.session.expiresAt - Date.now()) / 1000);
-    res.send({ access_token: req.session.publicToken, expires_in: expiresIn });
+server.get('/api/auth/token', checkAuth, async function (req, res) {
+    const { credentials } = req.session;
+    res.send({
+        access_token: credentials.access_token,
+        expires_in: Math.round((credentials.expires_at - Date.now()) / 1000)
+    });
 });
 
-server.get('/api/auth/profile', authMiddleware, async function (req, res) {
-    const profile = await getUserProfile(req.session.internalToken);
+server.get('/api/auth/profile', checkAuth, async function (req, res) {
+    const profile = await getUserProfile(req.session.credentials);
     res.send({ name: profile.name });
 });
 
-server.get('/api/hubs', authMiddleware, async function (req, res) {
-    const hubs = await getHubs(req.session.internalToken);
-    res.send(hubs);
+server.get('/api/hubs', checkAuth, async function (req, res) {
+    res.send(await getHubs(req.session.credentials));
 });
 
-server.get('/api/hubs/:hub/projects', authMiddleware, async function (req, res) {
-    const projects = await getProjects(req.params.hub, req.session.internalToken);
-    res.send(projects);
+server.get('/api/hubs/:hub/projects', checkAuth, async function (req, res) {
+    res.send(await getProjects(req.params.hub, req.session.credentials));
 });
 
-server.get('/api/hubs/:hub/projects/:project/contents', authMiddleware, async function (req, res) {
-    const contents = await getProjectContents(req.params.hub, req.params.project, req.query.folder, req.session.internalToken);
-    res.send(contents);
+server.get('/api/hubs/:hub/projects/:project/contents', checkAuth, async function (req, res) {
+    res.send(await getProjectContents(req.params.hub, req.params.project, req.query.folder, req.session.credentials));
 });
 
-server.get('/api/hubs/:hub/projects/:project/contents/:item/versions', authMiddleware, async function (req, res) {
-    const versions = await getItemVersions(req.params.project, req.params.item, req.session.internalToken);
-    res.send(versions);
+server.get('/api/hubs/:hub/projects/:project/contents/:item/versions', checkAuth, async function (req, res) {
+    res.send(await getItemVersions(req.params.project, req.params.item, req.session.credentials));
 });
 
 server.listen(PORT, function () { console.log('Server listening at', server.url); });
